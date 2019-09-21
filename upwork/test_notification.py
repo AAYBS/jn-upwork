2b047fd1b57365e5f40a56c7eeebdc5759e5b664
rblcoder <35212670+rblcoder@users.noreply.github.com>
2019-09-21 09:58:01 +0530

    Change import: .notification to from notification

--- upwork/test_notification.py
+++ upwork/test_notification.py
@@ -1,6 +1,6 @@
 import datetime as dt
 import unittest
-from .notification import Job, Config, UpworkClient
+from notification import Job, Config, UpworkClient
 import json
 
 config = Config("test_configuration.ini").config
